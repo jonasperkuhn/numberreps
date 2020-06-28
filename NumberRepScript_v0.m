5d57e196e47c1079de31828255d93adfc7f35709
Laura Stoinski <67442648+LauraMaiS@users.noreply.github.com>
2020-06-28 15:23:51 +0200

    Update NumberRepScript_v0.m

--- NumberRepScript_v0.m
+++ NumberRepScript_v0.m
@@ -1,6 +1,21 @@
 % Experimentalskript 'Number representations' fuer Steuerung psychol.
 % Experimente 2020
 
-testVar1 = 2;
+% Vordefinieren der Grundparameter
+useScreen = 0;  % 0 = genuiner Bildschirm / 1 = externer Bildschirm
+bkgrCol = [ 128 128 128 ];
 
-test2 = 1
+% Vordefinieren der Textparameter
+txtCol = [255 200 200];
+IntroSize = 64;
+InstructSize = 30; 
+introText = 'Herzlich willkommen zu unserem Experiment!';
+Instruktionen = '  ';  %Ergänzen
+
+% Vordefinieren der Stimuli
+einsTxt = 'Eins'
+neunTxt = 'Neun'
+einsNr = 1
+neunNr = 9
+distractor = '#'
+
