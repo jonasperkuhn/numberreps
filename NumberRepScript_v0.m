e11ba44447be415f482a17edfe04eb30e267112d
Laura Stoinski <67442648+LauraMaiS@users.noreply.github.com>
2020-06-28 16:13:54 +0200

    Update NumberRepScript_v0.m

--- NumberRepScript_v0.m
+++ NumberRepScript_v0.m
@@ -7,15 +7,49 @@ bkgrCol = [ 128 128 128 ];
 
 % Vordefinieren der Textparameter
 txtCol = [255 200 200];
-IntroSize = 64;
-InstructSize = 30; 
-introText = 'Herzlich willkommen zu unserem Experiment!';
-Instruktionen = '  ';  %Ergänzen
+textSize = 30;
+introText = 'Herzlich willkommen zu unserem Experiment!'; % Instruktion Ergänzen
+respTime = 3  % sollen wir die maximale RespTime auch bei 3s belassen?
+
 
 % Vordefinieren der Stimuli
 einsTxt = 'Eins'
 neunTxt = 'Neun'
-einsNr = 1
-neunNr = 9
+einsNr = '1'
+neunNr = '9'
 distractor = '#'
+stimCol = [0 0 0]
+stimSize = 12; % Anpassen
 
+%ResultMatrix vorbereiten + VP NR
+resMatrix = zeros(nTrials, 5);
+iVp = input('Versuchspersonennummer: ');
+% eventuell auch:  iBlock = input('Blocknummer: ');
+
+
+% Positions-Matrix
+
+
+% Randomisierung der Bedingungen
+
+
+% Instruktionen  --------------------------------------------------------------------------------------------------------
+
+Screen('Preference', 'SkipSyncTests', 1);
+Screen('Preference','ConserveVRAM',64);
+[window, windowSize]=Screen('OpenWindow', useScreen, bkgrCol );
+
+%%% Textausgabe
+Screen('TextSize', window, txtSize );
+DrawFormattedText(window, introText, 'center', 'center', txtCol);
+Screen('Flip', window);
+
+KbStrokeWait;
+
+% Experiment--------------------------------------------------------------
+%% Zeige Fixationskreuz
+DrawFormattedText(window, '+', 'center', 'center', [0 0 0]);  % Ergänze: Fixationskreuz muss wieder verschwinden
+Screen('Flip', window);
+
+% Vorbereitung der Prime-Ausgabe 
+DrawFormattedText(window, textQuad, {%position%} , stimCol);
